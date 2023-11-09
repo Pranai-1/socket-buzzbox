1a36f18df9e59e5acc01ae79023816c62d55caf4
Pranai-1 <reddypranai2017@gmail.com>
2023-11-09 10:49:32 +0530

    Remove prisma folder from tracking

--- prisma/migrations/20231020142255_socket_onlineusers/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "OnlineUsers" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "socketId" TEXT NOT NULL,
-
-    CONSTRAINT "OnlineUsers_pkey" PRIMARY KEY ("id")
-);
